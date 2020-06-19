03cffbdd31d3d2b3a17f13fd4b3e3bc60506d0f1
Wolf Sellmer <rp.wolfsellmer@gmail.com>
2020-06-18 23:41:34 -0300

    modulo03

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const nunjucks = require('nunjucks')
+
+
+const server = express()
+server.use(express.static('public'))
+
+server.set("view engine", "html")
+nunjucks.configure("views", {
+    express:server
+})
+
+server.get ("/", function(req, res){
+    return res.render("about")
+})
+server.get ("/portfolio", function(req, res){
+    return res.render("portfolio")
+})
+server.listen(5000, function(){
+    console.log("server is running")
+})
+server.use(function(req, res) {
+    res.status(404).render("not-found");
+  });
