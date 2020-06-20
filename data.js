edbeaa2b7da91d4ed5405fe25978051a4f347bac
Wolf Sellmer <rp.wolfsellmer@gmail.com>
2020-06-20 18:19:50 -0300

    finalv1

---
+++ data.js
@@ -0,0 +1,41 @@
+module.exports=[
+    {
+        id:"GykTLqODQuU",
+        title:"Formulário Animado com JS puro e CSS Animation | Mayk Brito",
+        duration: "57 mim",
+        price:"Free",
+        featured: true
+    },
+    {
+        id:"GykTLqODQuU",
+        title:"Formulário Animado com JS puro e CSS Animation | Mayk Brito",
+        duration: "57 mim",
+        price:"Free"
+    },
+    {
+        id:"GykTLqODQuU",
+        title:"Formulário Animado com JS puro e CSS Animation | Mayk Brito",
+        duration: "57 mim",
+        price:"Free",
+        featured: true
+    },
+    {
+        id:"GykTLqODQuU",
+        title:"Formulário Animado com JS puro e CSS Animation | Mayk Brito",
+        duration: "57 mim",
+        price:"Free"
+    },
+    {
+        id:"GykTLqODQuU",
+        title:"Formulário Animado com JS puro e CSS Animation | Mayk Brito",
+        duration: "57 mim",
+        price:"Free",
+        featured: true
+    },
+    {
+        id:"GykTLqODQuU",
+        title:"Formulário Animado com JS puro e CSS Animation | Mayk Brito",
+        duration: "57 mim",
+        price:"Free"
+    }
+]
--- server.js
+++ server.js
@@ -3,18 +3,49 @@ const nunjucks = require('nunjucks')
 
 
 const server = express()
+const videos = require("./data")    
+
 server.use(express.static('public'))
 
-server.set("view engine", "html")
+server.set("view engine", "njk")
 nunjucks.configure("views", {
-    express:server
+    express:server,
+    autoescape:false,
+    noCache:true
 })
 
 server.get ("/", function(req, res){
-    return res.render("about")
+    const about={
+        avatar_url:"https://avatars2.githubusercontent.com/u/65407620?s=460&u=5a0305e5473c02c541fccc2981bece23c3c8a183&v=4",
+        name:"Wolf Sellmer",
+        role:"Executivo Comercial",
+        description: 'Estudante de HTML, CSS e JavaScrip. Executivo Comercial para venda de Serviços. Estudante da <a href="https://Rocketseat.com.br" target="_blank">Rocketseat</a>',
+        links:[
+            { name: "GitHub", url: "https://github.com/wolfsellmer"},
+            { name: "LinkedIn", url: "https://www.linkedin.com/in/wolfsellmer/"},
+            { name: "Twitter", url: "https://twitter.com"},
+        ]
+    }
+
+
+    return res.render("about", {about})
 })
 server.get ("/portfolio", function(req, res){
-    return res.render("portfolio")
+
+
+    return res.render("portfolio", {items: videos})
+})
+server.get("/video", function(req, res){
+    const id= req.query.id
+    const video = videos.find(function(video){
+       return video.id ==id
+    })
+    if (!video){
+        return res.send("Video not found!")
+    }
+    return res.render("video", {item: video})
+
+     
 })
 server.listen(5000, function(){
     console.log("server is running")
